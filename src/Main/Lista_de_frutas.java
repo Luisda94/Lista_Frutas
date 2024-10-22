ae5992fe7994ded3a8ae80bef0ce7e19e4d89bd6
Luisda <luisda915@gmail.com>
2024-10-21 22:17:11 -0300

    Creando una lista en java(aprendiendo areglos lineales).

---
+++ src/Main/Lista_de_frutas.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class Lista_de_frutas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Este proyecto es para saber como a hacer areglos usaremos el ejemplo de realizar una lista de frtas*
+ */
+		/*¿QUE ES UN AREGLO? = Es un conjuto de datos del mismo tipo.
+		 * estos datos se guardan de forma lineal*/
+		/*int[] arreglo1;
+arreglo1 =new int[4];
+*/
+		int [] ListaFrutas;
+		
+		int ListaFrutas1 = new ListaFrutas1 [5];
+
+}
+}

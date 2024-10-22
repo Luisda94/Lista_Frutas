b84011e632c50297f389366f7ed4f867c24e0807
Luisda <luisda915@gmail.com>
2024-10-22 18:29:17 -0300

    add 2 matrices bidimensionales

--- src/Arreglos_bidemensionales/arreglos_bidimensionales.java
+++ src/Arreglos_bidemensionales/arreglos_bidimensionales.java
@@ -17,17 +17,43 @@ public class arreglos_bidimensionales {
 		arregloBidimensional [0][0] = 1 ;
 		arregloBidimensional [0][1] = 2;
 		arregloBidimensional [0][2] =3 ;
-		arregloBidimensional [1][1] =4 ;
-		arregloBidimensional [1][2] = 5;
+		arregloBidimensional [1][0] =4 ;
+		arregloBidimensional [1][1] = 5;
 		//Dato tanto la fila como la columna parten en el indice 0
-		
+		//si no se declara todo los valores java interpreta el resto con null o cero
 		//se comienza a hacer el recorrido con for
 		
 		for (int I=0;I<arregloBidimensional.length; I++) {
-			System.out.println(arregloBidimensional[0][I]);
+			for (int F=0;F<arregloBidimensional[I].length; F++)
+			System.out.println(arregloBidimensional[I][F]);
+		}
+		// segunda forma : //4 filas 4 columnas datos llenos sin null o cero
+		int[][] numeros = {{1,5,9,6},
+							{8,6,15,7}};
+		for (int i = 0; i < numeros.length; i++) {  // Recorre las filas
+		    for (int j = 0; j < numeros[i].length; j++) {  // Recorre las columnas
+		        System.out.println("Elemento en [" + i + "][" + j + "] es: " + numeros[i][j]);
+		    }
 		}
 		
 		
+		///
+		
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+            
+            // Recorrer solo hasta la fila 1.
+            for (int i = 0; i <= 1; i++) { // Cambia el límite superior para incluir solo hasta la fila 1
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.print(matriz[i][j] + " ");
+                }
+                System.out.println(); // Nueva línea después de cada fila
+
+		
 	}
 
+}
 }

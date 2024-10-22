adf262c062e644a699a8c0d18a4f7d38b99b4ce2
Luisda <luisda915@gmail.com>
2024-10-22 19:55:40 -0300

    Add arreglo y se define tamaño Add random

--- src/general/Arreglos.java
+++ src/general/Arreglos.java
@@ -1,10 +1,15 @@
 package general;
+import java.util.Random;  // Importamos Random para generar números aleatorios
 
 public class Arreglos {
+    public static void main(String[] args) {
+        
+        // Creamos un arreglo (una fila de 10 cajitas donde guardaremos números)
+        int[] arreglo = new int[10]; 
+        
+        // Creamos un "random", que es como una máquina de números sorpresa
+        Random random = new Random();  
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+     
+    }
 }

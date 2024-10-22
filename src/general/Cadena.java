b0a57662dcf0f44d293d41ce9e28f164660e1e84
Luisda <luisda915@gmail.com>
2024-10-22 20:31:40 -0300

    Se crea clase para saber si una palabra es palindromo

---
+++ src/general/Cadena.java
@@ -0,0 +1,20 @@
+package general;
+
+import java.util.Scanner;
+
+public class Cadena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.println("INgrese la palabra");
+String palabra = sc.nextLine();
+String reverso = new StringBuilder(palabra).reverse().toString();
+if (palabra.equalsIgnoreCase(reverso)) {
+System.out.println("es palindromo");
+	}else {
+		System.out.println("no es palindromo");
+	}
+sc.close();
+}
+}

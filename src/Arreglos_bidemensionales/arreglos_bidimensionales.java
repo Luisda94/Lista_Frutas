55b534c1a9ff03e6f91bf1410703ab020daa474c
Luisda <luisda915@gmail.com>
2024-10-22 17:50:04 -0300

    Add new arreglo tipo matriz

--- src/Arreglos_bidemensionales/arreglos_bidimensionales.java
+++ src/Arreglos_bidemensionales/arreglos_bidimensionales.java
@@ -3,8 +3,23 @@ package Arreglos_bidemensionales;
 public class arreglos_bidimensionales {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* TODO Auto-generated method stub
+		Los arreglos bidimensionales son una tabla con filas y columnas(dos dimensiones) 
+		Es similar al concepto de matrices en matematica.
+		*/
+		//para declarar un Array Bidimensional se puede hacer de las siguientes formas:
+		// uno instanciando el arreglo y colocando el tamaño de la fila y el tamaño de la columna.
+		//dato la fila siempre es el primero de izquierda a derecha el primero de derecha a izquierda.
+		int[][] arregloBidimensional = new int[3][3];
+		//en este caso tenemos que asignarles un valor a cada uno de los indices que hay en este arreglo
+		//en este caso son 3 datos para la fila y 3 datos para la columna.
+		//nombre de arreglos | nro fila |nro columna| valor asignado
+		arregloBidimensional [0][0] = 1 ;
+		arregloBidimensional [0][1] = 2;
+		arregloBidimensional [0][2] =3 ;
+		arregloBidimensional [1][1] =4 ;
+		arregloBidimensional [1][2] = 5;
+		
 	}
 
 }

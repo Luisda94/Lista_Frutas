74396ca48d8aa7f8724bf0a478b6c8d261b043c3
Luisda <luisda915@gmail.com>
2024-10-22 21:28:10 -0300

    add replaceAll, se reemplazan variables para hacer efectivo el reconocimiento de oraciones en palindromo

--- src/general/Cadena.java
+++ src/general/Cadena.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class Cadena {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
-System.out.println("INgrese la palabra");
-String palabra = sc.nextLine();
-String reverso = new StringBuilder(palabra).reverse().toString();
-String nuevoreverso = reverso.replaceAll(palabra, reverso);
-if (palabra.equalsIgnoreCase(reverso)) {
-System.out.println("es palindromo");
-	}else {
-		System.out.println("no es palindromo");
-	}
-sc.close();
-}
+    public static void main(String[] args) {
+        // Creamos un escáner para leer lo que el usuario escriba
+        Scanner sc = new Scanner(System.in);
+        
+        // Pedimos que ingrese una palabra
+        System.out.println("Ingrese la palabra: ");
+        String palabra = sc.nextLine();  // Guardamos la palabra que el usuario escribió
+
+        
+        String espaciador = palabra.replaceAll("[\\s]", "").toLowerCase(); // Eliminar espacios y convertir a minúsculas
+        
+        
+        // Creamos una versión de la palabra pero al revés
+        String reverso = new StringBuilder(espaciador).reverse().toString();
+       
+        if (espaciador.equalsIgnoreCase(reverso)) {
+            // Si son iguales, la palabra es un palíndromo
+            System.out.println("Es un palíndromo");
+        } else {
+            // Si no son iguales, no es un palíndromo
+            System.out.println("No es un palíndromo");
+        }
+        
+        // Cerramos el escáner para evitar fugas de memoria
+        sc.close();
+    }
 }
